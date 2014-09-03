a6f9b5f791ec5c5f5e88384a1c98b0419be13ab9
debojeet7 <debojeet.chatterjee@searce.com>
2014-09-03 21:45:27 +0530

    First Pus h

---
+++ app/src/main/java/com/searce/musicplayer/Splash.java
@@ -0,0 +1,15 @@
+package com.searce.musicplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by root on 3/9/14.
+ */
+public class Splash extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_splash);
+    }
+}
